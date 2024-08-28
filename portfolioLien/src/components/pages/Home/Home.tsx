27614c956aaa06a03a520083fc26d10fc6f5508c
Casper Van Damme <casper.van.damme@student.howest.be>
2024-08-28 12:08:09 +0200

    arrows added

--- portfolioLien/src/components/pages/Home/Home.tsx
+++ portfolioLien/src/components/pages/Home/Home.tsx
@@ -3,11 +3,12 @@ import Header from "../../basics/Header/Header";
 const Home = () => {
   return (
     <Header>
-      <div className="w-screen h-screen -mt-24 grid content-center justify-items-center text-left">
-        <div className="leading-tight">
+      <div className="w-screen h-screen -mt-24 grid justify-items-center text-left">
+        <div className="leading-tight place-self-center">
           <h1 className="text-primary text-10xl font-ivyMode">Hallo,</h1>
           <h2 className="text-5xl font-ivyMode">IK BEN LIEN VAN DEN ABBEELE</h2>
         </div>
+        <img src="/double_arrow.svg" alt="" className="fixed bottom-4 animate-bounce" />
       </div>
     </Header>  
   )
