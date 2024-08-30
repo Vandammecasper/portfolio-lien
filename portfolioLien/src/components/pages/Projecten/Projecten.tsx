f0e39e395b7c8b77575ab8f3793e2f0de7dcf99e
Casper Van Damme <casper.van.damme@student.howest.be>
2024-08-30 14:59:32 +0200

    routing for design and fotografie added

---
+++ portfolioLien/src/components/pages/Projecten/Projecten.tsx
@@ -0,0 +1,11 @@
+import Header from "../../basics/Header/Header";
+
+const Projecten = () => {
+  return (
+  <Header>
+    <h1>Projecten</h1>
+  </Header>
+);
+};
+
+export default Projecten;
--- portfolioLien/src/routes.tsx
+++ portfolioLien/src/routes.tsx
@@ -1,11 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
+import Projecten from "./components/pages/Projecten/Projecten";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Home />
-    }
+    },
+    {
+        path: "/design",
+        element: <Projecten />
+    },
+    {
+        path: "/fotografie",
+        element: <Projecten />
+    },
 ])
 
 export default router
