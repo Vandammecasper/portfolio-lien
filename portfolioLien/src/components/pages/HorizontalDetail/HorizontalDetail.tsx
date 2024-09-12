8ff485ae3eb006475705c13daa09ae242382fd20
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-12 11:54:41 +0200

    horizontal detail page responsive

--- portfolioLien/src/components/pages/HorizontalDetail/HorizontalDetail.tsx
+++ portfolioLien/src/components/pages/HorizontalDetail/HorizontalDetail.tsx
@@ -16,12 +16,12 @@ const HorizontalDetail = () => {
     if(design)
     return (
       <Header>
-        <div className="px-28 pr-80 mt-20 gap-20">
-          <h1 className="text-5xl font-ivyMode overflow-hidden text-black ">{design.name}</h1>
-          <p className="text-2xl text-black mt-6">{design.description}</p>
+        <div className="px-10 sm:px-20 md:px-28 xl:pr-80 mt-20 gap-20">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-ivyMode overflow-hidden text-black ">{design.name}</h1>
+          <p className="text-lg sm:text-xl lg:text-2xl text-black mt-6">{design.description}</p>
           <div className="flex flex-col items-end mt-4">
-            <img src={design.details} alt="" className="h-100 w-100vh mt-8"/>
-            <img src="/portfolio_icon_green.svg" alt="" className="h-32 -mt-16 -mr-16"/>
+            <img src={design.details} alt="" className="h-44 sm:h-64 md:h-80 lg:h-100 w-100vh mt-8 mr-0.5"/>
+            <img src="/portfolio_icon_green.svg" alt="" className="h-16 sm:h-20 md:h-24 lg:h-32 -mt-8 sm:-mt-10 md:-mt-12 lg:-mt-16 -mr-8 sm:-mr-10 md:-mr-12 lg:-mr-16"/>
           </div>
         </div>
       </Header>
@@ -31,12 +31,12 @@ const HorizontalDetail = () => {
     if(photo)
     return (
       <Header>
-        <div className="px-28 pr-80 mt-20 gap-20">
-          <h1 className="text-5xl font-ivyMode overflow-hidden text-black ">{photo.name}</h1>
-          <p className="text-2xl text-black mt-6">{photo.description}</p>
-          <div className="flex flex-col items-end">
-            <img src={photo.details} alt="" className="h-100 w-100vh mt-8"/>
-            <img src="/portfolio_icon_green.svg" alt="" className="h-32 -mt-16 -mr-16"/>
+        <div className="px-10 sm:px-20 md:px-28 xl:pr-80 mt-20 gap-20">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-ivyMode overflow-hidden text-black ">{photo.name}</h1>
+          <p className="text-lg sm:text-xl lg:text-2xl text-black mt-6">{photo.description}</p>
+          <div className="flex flex-col items-end mt-4">
+            <img src={photo.details} alt="" className="h-48 sm:h-64 md:h-80 lg:h-100 w-100vh mt-8 mr-0.5"/>
+            <img src="/portfolio_icon_green.svg" alt="" className="h-16 sm:h-20 md:h-24 lg:h-32 -mt-8 sm:-mt-10 md:-mt-12 lg:-mt-16 -mr-8 sm:-mr-10 md:-mr-12 lg:-mr-16"/>
           </div>
         </div>
       </Header>
