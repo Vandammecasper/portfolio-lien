c10e938b28995d1d53dbd8328a1e2dc6d0780fa4
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-16 13:53:51 +0200

    terugkeren in hoofdletter

--- portfolioLien/src/components/pages/HorizontalDetail/HorizontalDetail.tsx
+++ portfolioLien/src/components/pages/HorizontalDetail/HorizontalDetail.tsx
@@ -72,7 +72,7 @@ const HorizontalDetail = () => {
         <div className="w-screen absolute pl-10 sm:pl-20 md:pl-28 -mt-16 sm:-mt-20">
           <button onClick={handleGoBack} className="flex items-center justify-center gap-0 sm:gap-1 p-0.5 px-3 border-4 border-primary rounded-full">
             <img src="/arrow_left.svg" alt="" className="h-6 sm:h-8 -ml-2"/>
-            <p className="max-sm:text-sm">Terugkeren</p>
+            <p className="max-sm:text-sm">TERUGKEREN</p>
           </button>
         </div>
         <div className="px-10 sm:px-20 md:px-28 xl:pr-80 mt-28 gap-20 mb-8">
