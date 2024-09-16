0f0b7e96b9dfb1d41c0445d6dd554f786b8ac8b1
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-16 14:18:52 +0200

    roulette larger on wideview

--- portfolioLien/src/components/basics/PictureRoulette/PictureRoulette.tsx
+++ portfolioLien/src/components/basics/PictureRoulette/PictureRoulette.tsx
@@ -2,6 +2,6 @@
 
 export const PictureRoulette = ({img}: {img:string}) => {
   return (
-    <img className="w-100vh h-96" alt="" src={img} />
+    <img className="w-100vh h-96 2xl:h-110" alt="" src={img} />
   );
 };
