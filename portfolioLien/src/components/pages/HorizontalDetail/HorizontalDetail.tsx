7c6b0b1bbf3a069aa96f1d0025862fa60041ed40
Casper Van Damme <casper.van.damme@student.howest.be>
2024-10-17 14:08:52 +0200

    fixed responsiveness

--- portfolioLien/src/components/pages/HorizontalDetail/HorizontalDetail.tsx
+++ portfolioLien/src/components/pages/HorizontalDetail/HorizontalDetail.tsx
@@ -1,8 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import designs from "../../../utils/data/designs.json";
 import photos from "../../../utils/data/photos.json";
-import Marquee from "../../basics/Marquee/Marquee";
-import { PictureRoulette } from "../../basics/PictureRoulette/PictureRoulette";
 import { useState } from "react";
 
 
@@ -23,13 +21,24 @@ const Pictures = ({kind, name} : {kind: string, name:string}) => {
           prev < des.detailPictures.length - 1 ? prev + 1 : 0
         );
       }
-      return(
-        <div className="relative flex mt-4 w-full items-center justify-center overflow-hidden gap-16">
-          <img onClick={prevPicture} src="/arrow_left.svg" alt="" className="h-24" />
-          <img src={des.detailPictures[currentPicture]} alt="" className="h-96 2xl:h-110" />
-          <img onClick={nextPicture} src="/arrow_right.svg" alt="" className="h-24"/>
-        </div>
-      )
+      if(des.align == 'horizontal') {
+        return(
+          <div className="relative flex mt-4 w-full items-center justify-center overflow-hidden sm:gap-4 md:gap-10">
+            <img onClick={prevPicture} src="/arrow_left.svg" alt="" className="h-24" />
+            <img src={des.detailPictures[currentPicture]} alt="" className="h-32 sm:h-56 md:h-64 lg:h-96 2xl:h-110" />
+            <img onClick={nextPicture} src="/arrow_right.svg" alt="" className="h-24"/>
+          </div>
+        )
+      }
+      else if(des.align == 'vertical') {
+        return(
+          <div className="relative flex mt-4 w-full items-center justify-center overflow-hidden sm:gap-4 md:gap-10">
+            <img onClick={prevPicture} src="/arrow_left.svg" alt="" className="h-24" />
+            <img src={des.detailPictures[currentPicture]} alt="" className="h-60 sm:h-72 md:h-64 lg:h-96 2xl:h-110" />
+            <img onClick={nextPicture} src="/arrow_right.svg" alt="" className="h-24"/>
+          </div>
+        )
+      }
     } else {
       return (
         <div className="w-screen flex justify-center">
@@ -54,19 +63,39 @@ const Pictures = ({kind, name} : {kind: string, name:string}) => {
           prev < pho.detailPictures.length - 1 ? prev + 1 : 0
         );
       }
-      return(
-        <div className="relative flex mt-4 w-full items-center justify-center overflow-hidden gap-16">
-          <img onClick={prevPicture} src="/arrow_left.svg" alt="" className="h-24" />
-          <img src={pho.detailPictures[currentPicture]} alt="" className="h-96 2xl:h-110" />
-          <img onClick={nextPicture} src="/arrow_right.svg" alt="" className="h-24"/>
-        </div>
-      )
+      if(pho.align == 'horizontal') {
+        return(
+          <div className="relative flex mt-4 w-full items-center justify-center overflow-hidden sm:gap-4 md:gap-10">
+            <img onClick={prevPicture} src="/arrow_left.svg" alt="" className="h-24" />
+            <img src={pho.detailPictures[currentPicture]} alt="" className="h-32 sm:h-56 md:h-64 lg:h-96 2xl:h-110" />
+            <img onClick={nextPicture} src="/arrow_right.svg" alt="" className="h-24"/>
+          </div>
+        )
+      }
+      else if(pho.align == 'vertical') {
+        return(
+          <div className="relative flex mt-4 w-full items-center justify-center overflow-hidden sm:gap-4 md:gap-10">
+            <img onClick={prevPicture} src="/arrow_left.svg" alt="" className="h-24" />
+            <img src={pho.detailPictures[currentPicture]} alt="" className="h-60 sm:h-72 md:h-64 lg:h-96 2xl:h-110" />
+            <img onClick={nextPicture} src="/arrow_right.svg" alt="" className="h-24"/>
+          </div>
+        )
+      }
     } else {
-      return (
-        <div className="w-screen flex justify-center">
-          <img src={pho?.details} alt="" className="h-96 2xl:h-110" />
-        </div>
-      )
+      if (pho?.align == 'vertical'){
+        return (
+          <div className="w-screen flex justify-center">
+            <img src={pho?.details} alt="" className="h-96 2xl:h-110" />
+          </div>
+        )
+      }
+      else if (pho?.align == 'horizontal'){
+        return (
+          <div className="w-screen flex justify-center">
+            <img src={pho?.details} alt="" className="h-56 sm:h-80 md:h-96 2xl:h-110" />
+          </div>
+        )
+      }
     }
   }
 }
@@ -101,7 +130,7 @@ const HorizontalDetail = () => {
         </div>
         <div className="px-10 sm:px-20 md:px-28 xl:pr-80 mt-28 gap-20 mb-8 2xl:mb-16">
           <div className="flex items-end">
-            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-ivyMode overflow-hidden text-black ">{design.name}</h1>
+            <h1 className="text-2xl sm:text-4xl lg:text-5xl font-ivyMode overflow-hidden text-black ">{design.name}</h1>
             <img src="/portfolio_icon_green.svg" alt="" className="h-2.5 sm:h-3 lg:h-4 ml-1.5 max-sm:mb-0.5"/>
           </div>
           <p className="text-lg sm:text-xl lg:text-2xl text-black mt-6">{design.description}</p>
